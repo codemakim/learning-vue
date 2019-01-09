3f3701369004fb3579753cc6e7331fe2cd4904f8
jhkim <jhkim@asnet.co.kr>
2019-01-09 13:45:39 +0900

    TODO 앱 작성 중 - todoList

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,13 +1,55 @@
 <template>
-    <div>input</div>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" placeholder="Type what you have to do">
+        <span v-on:click="addTodo" class="addContainer addButton">+</span>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            newTodoItem: ''
+        }
+    },
+    methods: {
+        addTodo() {
+            var value = this.newTodoItem.trim();
+            if(value != '') {
+                localStorage.setItem(value, value);
+                this.clearInput();
+            }
+        },
+        clearInput() {
+            this.newTodoItem = '';
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+    input:focus {
+        outline: none;
+    }
+    .inputBox {
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input {
+        border-style: none;
+        font-size: 0.9rem;
+    }
+    .addContainer {
+        float: right;
+        background: linear-gradient(to right, #6478FB, #8763FB);
+        display: inline-block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+    .addButton {
+        color: white;
+        vertical-align: middle;
+    }
 </style>
--- vue-todo/src/components/todoList.vue
+++ vue-todo/src/components/todoList.vue
@@ -1,10 +1,59 @@
 <template>
-    <div>list</div>
+    <section>
+        <ul>
+            <li v-for="todoItem in todoItems" class="shadow">
+                <i class="checkButton fa fa-check" aria-hidden="true"></i>
+                {{todoItem}}
+                <span type="button" v-on:click="removeTodo(todoItem, index)" class="removeButton">
+                    <i class="fa fa-trash" aria-hidden="true"></i>
+                </span>
+            </li>
+        </ul>
+    </section>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            todoItems: []
+        }
+    },
+    created() {
+        if(localStorage.length > 0) {
+            for(var i=0; i<localStorage.length; i++) {
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    }
 }
 </script>
+
+<style>
+    ul {
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+    li {
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding:0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+    .checkButton {
+        line-height: 50px;
+        color:#62acde;
+        margin-right: 5px;
+    }
+    .removeButton {
+        margin-left: auto;
+        color: #de4343;
+    }
+</style>
 
