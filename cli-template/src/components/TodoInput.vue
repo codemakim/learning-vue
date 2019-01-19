7cec7be5bce4a0dc25ac3b9a651551b3339b9be8
codemakim <kaeaoga@gmail.com>
2019-01-19 22:44:50 +0900

    06-4장까지 완료. 실시간으로 아이템이 갱신되지 않음.

--- cli-template/src/components/TodoInput.vue
+++ cli-template/src/components/TodoInput.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        <input type="text" v-model="newTodoItem">
-        <button v-on:click="addTodo">추가</button>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" placeholder="Type what you have to do">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+        </span>
     </div>
 </template>
 
@@ -14,11 +16,42 @@ export default {
     },
     methods: {
         addTodo() {
-            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            let value = this.newTodoItem.trim();
+            if(value !== '') {
+                localStorage.setItem(value, value);
+                this.clearInupt();
+            }
+        },
+        clearInupt() {
+            this.newTodoItem = '';
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
+    input:focus {
+        outline: none;
+    }
+    .inputBox {
+        background:white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input {
+        border-style: none;
+        font-size: 0.9rem;
+    }
+    .addContainer {
+        float: right;
+        background: linear-gradient(to right, #6478FB, #8763FB);
+        display: inline-block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+    .addBtn {
+        color: white;
+        vertical-align: middle;
+    }
 </style>
--- cli-template/src/components/TodoList.vue
+++ cli-template/src/components/TodoList.vue
@@ -1,13 +1,63 @@
 <template>
-    <div>list</div>
+    <section>
+        <ul>
+            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
+                <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+                {{ todoItem }}
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                    <i class="fa fa-trash" aria-hidden="true"></i>
+                </span>
+            </li>
+        </ul>
+    </section>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            todoItems: []
+        }
+    },
+    methods: {
+        removeTodo(todoItem, index) {
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index,1);
+        }
+    },
+    created() {
+        if(localStorage.length > 0) {
+            for(var i=0; i<localStorage.length; i++) {
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    },
 }
 </script>
 
 <style>
-
+    ul {
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+    li {
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        background: white;
+        border-radius: 5px;
+    }
+    .checkBtn {
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+    .removeBtn {
+        margin-left: auto;
+        color: #de4343;
+    }
 </style>
